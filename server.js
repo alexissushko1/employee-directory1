2e63bcbbae32adb66bef93d75546efc1e3d7fe82
Alexis Sushko <alexis.sushko@gmail.com>
2024-10-07 14:37:53 -0400

    Created app to show an array of employees and a random person from the array of employees

--- server.js
+++ server.js
@@ -1,1 +1,36 @@
 // TODO: this file :)
+
+const express = require("express");
+const app = express();
+
+const PORT = 3000;
+
+const employees = require("./employees");
+
+app.get("/", (req, res) => {
+  res.send("Hello employees!");
+});
+
+app.get("/employees", (req, res) => {
+  res.json(employees);
+})
+
+app.get("/employees/random", (req, res) => {
+ const random = Math.floor(Math.random() * employees.length);
+const randomEmployee = employees[random];
+ res.json(randomEmployee.name);
+})
+
+app.get("/employees/:id", (req, res) => {
+  const employee = employees.find((obj) => obj.id === +req.params.id);
+  if(employee){
+    res.send(employee.name);
+  } else {
+    res.send("Not an employee");
+  }
+}),
+
+
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}.`);
+});
